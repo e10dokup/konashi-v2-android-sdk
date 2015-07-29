0fc310c904b658fae48e60013085fb830414fc2a
e10dokup <e10dokup@stu.kobe-u.ac.jp>
2015-07-29 21:11:43 +0900

    fix #7 (fix up incorrect AIO1 and AIO2)

--- Konashi/konashi-v2-android-sdk/src/main/java/com/uxxu/konashi/lib/KonashiBaseManager.java
+++ Konashi/konashi-v2-android-sdk/src/main/java/com/uxxu/konashi/lib/KonashiBaseManager.java
@@ -475,11 +475,11 @@ public class KonashiBaseManager implements BluetoothAdapter.LeScanCallback, OnBl
                     onUpdateAnalogValue(Konashi.AIO0, value);
                 }
                 else if(uuid.equals(KonashiUUID.ANALOG_READ1_UUID)){
-                    value = (characteristic.getValue()[0]<<8 & 0xFF) | (characteristic.getValue()[1] & 0xFF);
+                    value = (characteristic.getValue()[0]<<8 & 0xFF00) | (characteristic.getValue()[1] & 0xFF);
                     onUpdateAnalogValue(Konashi.AIO1, value);
                 }
                 else if(uuid.equals(KonashiUUID.ANALOG_READ2_UUID)){
-                    value = (characteristic.getValue()[0]<<8 & 0xFF) | (characteristic.getValue()[1] & 0xFF);
+                    value = (characteristic.getValue()[0]<<8 & 0xFF00) | (characteristic.getValue()[1] & 0xFF);
                     onUpdateAnalogValue(Konashi.AIO2, value);
                 }
                 else if(uuid.equals(KonashiUUID.BATTERY_LEVEL_UUID)){
